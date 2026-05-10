87ee304e9e102d443f8cfbd5db304b7772a97fbd
João Pedro Rosa <joao.rosa5@outlook.com>
2026-05-09 23:22:23 -0300

    N06 - Migração do servidor HTTP para Express com definição de rotas GET

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Seja Bem vindo!</h1>");
+
     console.log("URL:", req.url);
     console.log("METHOD:", req.method);
-
-    switch (req.url) {
-        case "/":
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "text/html");
-            res.end("<h1>Seja Bem vindo!</h1>");
-            break;
-
-        case "/users":
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            res.end(
-                JSON.stringify({
-                    users: [
-                        {
-                            name: "João Rosa",
-                            email: "joao.rosa@saipos.com",
-                            id: 1,
-                        },
-                    ],
-                }),
-            );
-    }
 });
 
-server.listen(3000, "127.0.0.1", () => {
+app.get("/users", (req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+        users: [
+            {
+                name: "João Rosa",
+                email: "joao.rosa@saipos.com",
+                id: 1,
+            },
+        ],
+    });
+});
+
+app.listen(3000, "127.0.0.1", () => {
     console.log("Servidor rodando!");
 });
