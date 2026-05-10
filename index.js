855eefb20cf6771dd77f5b2f889273e4b45c6708
João Pedro Rosa <joao.rosa5@outlook.com>
2026-05-09 23:03:57 -0300

    N04 - Implementação de rotas HTTP com respostas HTML e JSON no servidor NodeJS

--- index.js
+++ index.js
@@ -2,7 +2,29 @@ const http = require("http");
 const server = http.createServer((req, res) => {
     console.log("URL:", req.url);
     console.log("METHOD:", req.method);
-    res.end("OK");
+
+    switch (req.url) {
+        case "/":
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.end("<h1>Seja Bem vindo!</h1>");
+            break;
+
+        case "/users":
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.end(
+                JSON.stringify({
+                    users: [
+                        {
+                            name: "João Rosa",
+                            email: "joao.rosa@saipos.com",
+                            id: 1,
+                        },
+                    ],
+                }),
+            );
+    }
 });
 
 server.listen(3000, "127.0.0.1", () => {
