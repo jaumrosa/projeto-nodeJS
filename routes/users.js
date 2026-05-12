ca7d17dc1aac2e37c9e1fef6f4a2a8b308dbc3cc
João Pedro Rosa <joao.rosa5@outlook.com>
2026-05-11 22:34:41 -0300

    N12 - Refatoração de rotas com app.route() e centralização do tratamento de erros com pasta utils

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,11 @@ const db = new Datastore({
 });
 
 module.exports = (app) => {
-    app.get("/users", (req, res) => {
+    const route = app.route("/users");
+    route.get((req, res) => {
         db.find({}).sort({ name: 1 }).exec((err, users) => {
                 if (err) {
-                    console.log(`error: ${err}`);
-                    res.status(400).json({
-                        error: err,
-                    });
+                    app.utils.error.send(err, req, res);
                 } else {
                     res.statusCode = 200;
                     res.setHeader("Content-Type", "application/json");
@@ -22,13 +20,10 @@ module.exports = (app) => {
             });
     });
 
-    app.post("/users", (req, res) => {
+    route.post((req, res) => {
         db.insert(req.body, (err, user) => {
             if (err) {
-                console.log(`error: ${err}`);
-                res.status(400).json({
-                    error: err,
-                });
+                app.utils.error.send(err, req, res);
             } else {
                 res.status(200).json(user);
             }
