a0302810e6ee99ba45d0f8f233adf741645635b6
João Pedro Rosa <joao.rosa5@outlook.com>
2026-05-11 22:26:26 -0300

    N11 - Implementação de listagem de usuários com consulta e ordenação no NeDB

--- routes/users.js
+++ routes/users.js
@@ -6,17 +6,20 @@ const db = new Datastore({
 
 module.exports = (app) => {
     app.get("/users", (req, res) => {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.json({
-            users: [
-                {
-                    name: "João Rosa",
-                    email: "joao.rosa@saipos.com",
-                    id: 1,
-                },
-            ],
-        });
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
+                if (err) {
+                    console.log(`error: ${err}`);
+                    res.status(400).json({
+                        error: err,
+                    });
+                } else {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json({
+                        users,
+                    });
+                }
+            });
     });
 
     app.post("/users", (req, res) => {
